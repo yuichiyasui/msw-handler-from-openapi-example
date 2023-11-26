57691c0568744182e0b5a4f2756148643889753e
yuichiyasui <yassan.b.a.w@gmail.com>
2023-11-26 19:50:29 +0900

    Add orval

---
+++ orval.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "orval";
+export default defineConfig({
+  petstore: {
+    input: "./openapi/petstore.yaml",
+    output: {
+      mode: "tags-split",
+      target: "src/api/__generated__/petstore.ts",
+      schemas: "./src/api/__generated__",
+      prettier: true,
+      clean: true,
+      client: "react-query",
+      mock: true,
+      override: {
+        mutator: {
+          path: "./src/api/custom-instance.ts",
+          name: "customInstance",
+        },
+      },
+    },
+  },
+});
