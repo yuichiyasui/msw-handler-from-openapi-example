e2a318fb645761a32926deb5657b69d7ba4f9f9c
yuichiyasui <yassan.b.a.w@gmail.com>
2023-11-26 19:22:16 +0900

    Add react-query

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+import type { Preview } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={new QueryClient()}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
+};
+
+export default preview;
